a77c34d88182e439df7a75b7557ee7abdbee3c14
EmojiTvYt2 <106467462+EmojiTvYt2@users.noreply.github.com>
2022-05-29 10:49:33 +0200

    v1.1.1 Update
    More info, some fixes ect.

--- WindowsAppUpdater.bat
+++ WindowsAppUpdater.bat
@@ -1,13 +1,13 @@
 @echo off
-title Apps Updater / By: EMOJI TV YT
+title WindowsAppUpdater / By: EMOJI TV YT
 color 0E
 
 :start
 cd /D C:\
 cls
-echo ============================
-echo === Windows Apps Updater ===
-echo ============================
+echo ===========================
+echo === Windows App Updater ===
+echo ===========================
 echo.
 echo Select an Option
 echo.
@@ -22,24 +22,49 @@ if %cho%==3 exit
 
 :updateapps
 cls
+echo ===========================
+echo === Windows App Updater ===
+echo ===========================
+echo.
 echo Apps will start updateing in 5 seconds...
 ping localhost -n 1 >nul
 cls
+echo ===========================
+echo === Windows App Updater ===
+echo ===========================
+echo.
 echo Apps will start updateing in 4 seconds...
 ping localhost -n 1 >nul
 cls
+echo ===========================
+echo === Windows App Updater ===
+echo ===========================
+echo.
 echo Apps will start updateing in 3 seconds...
 ping localhost -n 1 >nul
 cls
+echo ===========================
+echo === Windows App Updater ===
+echo ===========================
+echo.
 echo Apps will start updateing in 2 seconds...
 ping localhost -n 1 >nul
 cls
+echo ===========================
+echo === Windows App Updater ===
+echo ===========================
+echo.
 echo Apps will start updateing in 1 second...
 ping localhost -n 1 >nul
 goto updateing
 
 :updateing
 cls
+echo ===========================
+echo === Windows App Updater ===
+echo ===========================
+echo.
+echo.
 winget upgrade --all
 echo.
 pause
@@ -51,19 +76,22 @@ goto start
 
 :info
 cls
-echo ============================
-echo === Windows Apps Updater ===
-echo ============================
+echo ===========================
+echo === Windows App Updater ===
+echo ===========================
 echo.
 echo Creator - EMOJI TV YT
-echo Program Version - 1.1
+echo Program Version - 1.1.1
 echo.
-echo ==== About  the Program ====
+echo ==== About the Program ====
 echo WindowsAppUpdater is Updateing
 echo ONLY apps that Microsoft added to the Update list
 echo.
 echo Apps like GIMP, Notepad++, Google Chrome ect. will be updated
 echo (The popular apps only will be updated)
+echo.
+echo Program will automaticly download and run Instalators
+echo for newest versions of applications that you have on your PC/Laptop
 echo.
 echo Click Any Key to Go Back to The Menu...
 pause >nul
